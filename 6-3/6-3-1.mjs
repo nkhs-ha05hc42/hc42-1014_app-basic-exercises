fcb2155ad36f0876355ce75dc97c6499e8a2e970
HC421014 <nkhshc421014@g.neec.ac.jp>
2024-12-23 13:38:06 +0900

    6-3-1

---
+++ 6-3/6-3-1.mjs
@@ -0,0 +1,3 @@
+const Array1 = [" A"," B"," C"]
+const newArray1 = Array1.map(Array => "★" + Array)
+console.log(newArray1)
---
+++ 6-3/6-3-4.mjs
@@ -0,0 +1,4 @@
+const array = ["A","B","C"]
+console.log(array)
+const arraystr = array.join("-")
+console.log(arraystr)
